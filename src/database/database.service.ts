9a3d2af58410ff736167de0ee42b79247f814a06
Tomas <tomas.ew88@gmail.com>
2024-11-19 21:00:50 +0200

    Connect to mongodb

---
+++ src/database/database.service.ts
@@ -0,0 +1,12 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import mongoose from 'mongoose';
+
+@Injectable()
+export class DatabaseService {
+  async onModuleInit() {
+      await this.connect();
+  }
+  public async connect() {
+    await mongoose.connect('mongodb://root:rootpassword@127.0.0.1:27017');
+  }
+}
