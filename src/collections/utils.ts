345fd0565ac9e36bf13e67a71828978d9cc3285b
Tomas <tomas.ew88@gmail.com>
2024-11-26 20:36:14 +0200

    Refactor parseSchema function

--- src/collections/utils.ts
+++ src/collections/utils.ts
@@ -9,24 +9,29 @@ const typeMap = {
   Boolean: Boolean,
 };
 
+function parseType(typeString: string) {
+  for (let typeKey in typeMap) {
+    if (typeString === typeKey) {
+      return typeMap[typeKey];
+    }
+  }
+  return null;
+}
+
 export function parseSchema(schemaStructure: {}) {
   const schema = structuredClone(schemaStructure);
   for (let key in schema) {
     // Will be set to false if we find a type key match
-    let isObject = true;
-    for (let typeKey in typeMap) {
-      if (schema[key] === typeKey) {
-        isObject = false;
-        schema[key] = typeMap[typeKey];
-      }
-
-      if (isObject) {
-        const obj = schema[key];
-        for (let typeKey in typeMap) {
-          if (obj['type'] === typeKey) {
-            obj['type'] = typeMap[typeKey];
-          }
-        }
+    const type = parseType(schema[key]);
+    if (type !== null) {
+      schema[key] = type;
+    } else {
+      const obj = schema[key];
+      const innerType = parseType(obj['type']);
+      if (innerType !== null) {
+        obj['type'] = innerType;
+      } else {
+        throw new Error('Unexpected schema structure!');
       }
     }
   }
