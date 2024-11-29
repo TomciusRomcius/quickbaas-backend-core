efea7a4a8d8ba7e315f08380ecd590f28f31062f
Tomas <tomas.ew88@gmail.com>
2024-11-29 18:21:32 +0200

    Move script to class member list

--- src/server-functions/features/server-function.ts
+++ src/server-functions/features/server-function.ts
@@ -4,21 +4,22 @@ import { Response, Request } from 'express';
 export default class ServerFunction {
   name: string;
   userCode: string;
+  script: vm.Script;
   context: {};
 
   constructor(name: string, userCode: string, context: {}) {
     this.name = name;
     this.userCode = userCode;
     this.context = context;
+    this.script = new vm.Script(this.userCode);
   }
 
   public run(req: Request, res: Response) {
-    const script = new vm.Script(this.userCode);
     const contextifiedSandbox = vm.createContext({
       ...this.context,
       req: req,
       res: res,
     });
-    script.runInContext(contextifiedSandbox);
+    this.script.runInContext(contextifiedSandbox);
   }
 }
