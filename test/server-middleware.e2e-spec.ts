341abd610559a4755a3aedc390d6215afd6758b8
Tomas <tomas.ew88@gmail.com>
2024-12-01 14:08:44 +0200

    E2E test

---
+++ test/server-middleware.e2e-spec.ts
@@ -0,0 +1,66 @@
+import mongoose from 'mongoose';
+import { config } from 'dotenv';
+import ServerMiddleware from 'src/common/models/serverMiddlewareModel';
+import * as request from 'supertest';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from 'src/app.module';
+import { INestApplication } from '@nestjs/common';
+
+describe('Server middleware with database-client test', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    await mongoose.connect(process.env.DATABASE_URLS);
+    await ServerMiddleware.deleteMany();
+
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('should succesfully create middleware', async () => {
+    const payload = {
+      name: 'middlewaretest',
+      code: 'console.log("test")',
+      executesOn: {
+        database: true,
+        auth: true,
+      },
+    };
+    const res = await request(app.getHttpServer())
+      .post('/server-middleware/create')
+      .send(payload);
+    expect(res.status).toBe(201);
+    const retrieved = await ServerMiddleware.findOne({ name: payload.name });
+    expect(retrieved).toBeTruthy();
+    expect(retrieved.name).toBe(payload.name);
+    expect(retrieved.code).toBe(payload.code);
+  });
+
+  it('should run the middleware on a database call', async () => {
+    const payload = {
+      name: 'middlewaretest',
+      code: 'res.status(400).json("custom"); cancelRequest()',
+      executesOn: {
+        database: true,
+        auth: true,
+      },
+    };
+
+    const dbPayload = {
+      path: 'users',
+    };
+    await request(app.getHttpServer())
+      .post('/server-middleware/create')
+      .send(payload);
+
+    const res = await request(app.getHttpServer())
+      .post('/database-client/get')
+      .send(dbPayload);
+    expect(res.status).toBe(400);
+    expect(res.body).toBe('custom');
+  });
+});
