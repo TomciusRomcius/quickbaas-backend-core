62a40f68f8c837cb958a69dcf466b41898705685
Tomas <tomas.ew88@gmail.com>
2024-12-08 10:06:53 +0200

    Add the ability to connect to multiple mongoose instances

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,12 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import mongoose from 'mongoose';
 
 @Injectable()
 export class DatabaseService {
+  constructor(private configService: ConfigService) {}
+
   async onModuleInit() {
     await this.connect();
   }
   public async connect() {
-    await mongoose.connect('mongodb://root:rootpassword@127.0.0.1:27017');
+    let urls = this.configService.get('DATABASE_URLS');
+    const pendingConnections = [];
+    if (urls) {
+      (urls as string).split(' ').forEach((url) => {
+        const connect = mongoose.connect(url);
+        pendingConnections.push(connect);
+      });
+    }
+
+    await Promise.all(pendingConnections);
   }
 }
