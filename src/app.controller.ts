90965a453049986f586af1672df7546087b8df7b
Tomas <tomas.ew88@gmail.com>
2024-12-08 10:05:31 +0200

    Add a wipe-test-db api route for tests

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import mongoose from 'mongoose';
+import { AdminGuard } from './common/utils/admin.guard';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,25 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('wipe-test-db')
+  @UseGuards(AdminGuard)
+  async wipeTestDb() {
+    if (process.env.NODE_ENV === 'production') return;
+    const collectionNames = (
+      await mongoose.connection.db.listCollections().toArray()
+    ).map((col) => col.name);
+
+    let deletions = [];
+    for (const name of collectionNames) {
+      const deletion = async () => {
+        console.log(`Deleting ${name}`);
+        const col = mongoose.connection.db.collection(name);
+        await col.deleteMany();
+      };
+      deletions.push(deletion());
+    }
+    await Promise.all(deletions);
   }
 }
