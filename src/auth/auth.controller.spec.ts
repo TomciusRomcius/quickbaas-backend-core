53e04d04f28eb7da29b60d2569dd2536fd57089a
Tomas <tomas.ew88@gmail.com>
2024-12-18 11:43:13 +0200

    Basic controller test

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -4,11 +4,22 @@ import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let mockAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    mockAuthService = {
+      signInWithPassword: jest.fn(),
+      signUpWithPassword: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
@@ -16,5 +27,17 @@ describe('AuthController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should call signUpWithPassword on the service', async () => {
+    const dto = { email: 'email@gmail.com', password: 'password' };
+    controller.signUpWithPassword(dto);
+    expect(mockAuthService.signUpWithPassword).toHaveBeenCalledWith(dto);
+  });
+
+  it('should call signInWithPassword on the service', async () => {
+    const dto = { email: 'email@gmail.com', password: 'password' };
+    controller.signInWithPassword(dto);
+    expect(mockAuthService.signInWithPassword).toHaveBeenCalledWith(dto);
   });
 });
