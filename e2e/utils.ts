3cc99943e1c331778c4d2e962b13808592d3ea0f
Tomas Rimkevicius <tomas.ew88@gmail.com>
2024-12-29 15:06:37 +0200

    Use an API route for wiping test dbs

--- e2e/utils.ts
+++ e2e/utils.ts
@@ -1,24 +1,13 @@
-import mongoose from 'mongoose';
-import { GenericContainer } from 'testcontainers';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
 
-let container;
-
-export async function connectToTestDbs() {
-  container = await new GenericContainer('mongo')
-    .withExposedPorts(27017)
-    .start();
-
-  const mongoUri = `mongodb://${container.getHost()}:${container.getMappedPort(27017)}`;
-  process.env.DATABASE_URLS = mongoUri;
-}
-
-// Can't be called before connectToTestDbs()
-export async function wipeTestDbs() {
-  const promises = [];
-
-  mongoose.connections.forEach((connection) => {
-    promises.push(connection.db.dropDatabase());
+export async function wipeTestDbs(app: INestApplication) {
+  const res = await request(app.getHttpServer()).post('/wipe-test-db').send({
+    adminKey: 'secret',
   });
-
-  await Promise.all(promises);
+  if (!(res.status === 200 || res.status === 201)) {
+    console.error(`Failed to delete test db \n 
+                  Response status: ${res.status} \n
+                  Response message: ${res.text}`);
+  }
 }
