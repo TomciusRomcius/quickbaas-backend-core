4b4bf59f0db2e26c5aa0ec4759ac3c0709012f16
Tomas Rimkevicius <tomas.ew88@gmail.com>
2025-01-02 14:07:22 +0200

    JWT refactor

--- src/common/utils/jwt.ts
+++ src/common/utils/jwt.ts
@@ -8,7 +8,7 @@ export default class JWT {
 
   public static verify(token: string) {
     let resultObject = {};
-    jwt.verify(token, (err, decoded) => {
+    jwt.verify(token, JWT.getAuthKey(), (err, decoded) => {
       if (err) {
         console.error(err);
       } else {
@@ -17,21 +17,6 @@ export default class JWT {
     });
 
     return resultObject;
-  }
-
-  public static parseJWT(jwtToken: string): {
-    header: any;
-    payload: any;
-    err?: 'tampered' | 'expired';
-  } {
-    // TODO: check for expiration and clean up err type
-    let err: 'tampered' | 'expired';
-    if (!JWT.verify(jwtToken)) {
-      err = 'tampered';
-    }
-    const [headerStr, payloadStr, signatureStr] = jwtToken.split('.');
-
-    return { header: atob(headerStr), payload: atob(payloadStr), err: err };
   }
 
   private static getAuthKey() {
