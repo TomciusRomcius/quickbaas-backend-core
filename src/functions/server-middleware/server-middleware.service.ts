70a152afd3a7ce20af7504ef4ae0e9244ac681ed
Tomas Rimkevicius <tomas.ew88@gmail.com>
2025-01-11 12:32:14 +0200

    Refresh middleware

--- src/functions/server-middleware/server-middleware.service.ts
+++ src/functions/server-middleware/server-middleware.service.ts
@@ -9,7 +9,31 @@ import { CreateMiddlewareDto } from './dtos/create-middleware-dto';
 export class ServerMiddlewareService {
   public middlewares: MiddlewareFunctionWrapper[] = [];
 
-  constructor(private databaseClientService: DatabaseClientOperationService) {}
+  // TODO: test this
+  public async refreshMiddlewareFunctions() {
+    const dbMiddlewares = await ServerMiddlewareModel.find({});
+    for (let dbMiddleware of dbMiddlewares) {
+      const foundMiddleware = this.middlewares.find((el) => el.getName());
+      // If middleware exists, replace the middlewares code if the code has changed
+      if (foundMiddleware) {
+        const foundFunction = foundMiddleware.getSandboxedFunction();
+        if (foundFunction.userCode !== dbMiddleware.code) {
+          foundMiddleware.setSandboxedFunction(
+            new SandboxedFunction(dbMiddleware.code),
+          );
+        }
+      }
+      // Else create new middleware
+      else {
+        const middleware = new MiddlewareFunctionWrapper(
+          dbMiddleware.name,
+          new SandboxedFunction(dbMiddleware.code),
+          dbMiddleware.runsOn,
+        );
+        this.middlewares.push(middleware);
+      }
+    }
+  }
 
   public async getAllMiddlewareNames() {
     const dbMiddlewares = await ServerMiddlewareModel.find({}, { name: true });
