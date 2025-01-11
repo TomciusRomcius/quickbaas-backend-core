b6f8eb9ca1f5dd7c27e853e563a654c93bcd0265
Tomas Rimkevicius <tomas.ew88@gmail.com>
2025-01-11 12:28:37 +0200

    Fix test

--- e2e/server-middleware.e2e.spec.ts
+++ e2e/server-middleware.e2e.spec.ts
@@ -1,4 +1,3 @@
-import ServerMiddleware from 'src/common/models/serverMiddlewareModel';
 import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
 import { AppModule } from 'src/app.module';
@@ -23,18 +22,33 @@ describe('Server middleware with database-client test', () => {
 
   it('should succesfully create middleware ', async () => {
     const payload = {
-      name: 'middlewaretest',
-      code: 'res.status(400).json("custom"); cancelRequest()',
-      runsOn: {
-        database: true,
-        auth: true,
-      },
+      middlewares: [
+        {
+          name: 'middlewaretest',
+          code: 'res.status(400).json("custom"); cancelRequest()',
+          runsOn: {
+            database: true,
+            auth: true,
+          },
+        },
+      ],
+      adminKey: 'secret',
     };
 
     const createRes = await request(app.getHttpServer())
       .post('/server-middleware/create')
       .send(payload);
     expect(createRes.status).toBe(201);
+  });
+
+  it('should succesfully get the created middleware', async () => {
+    const getRes = await request(app.getHttpServer())
+      .post('/server-middleware/get')
+      .send({
+        adminKey: 'secret',
+      });
+
+    expect((getRes.body as string[]).includes('middlewaretest')).toBe(true);
   });
 
   it('should run the middleware on a database call', async () => {
